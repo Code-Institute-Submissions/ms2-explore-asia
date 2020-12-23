6e0345238cf18580ec633688fd4c356553c87606
EllyC20 <callanelly@yahoo.com>
2020-12-23 11:06:31 +0000

    Add function to change text to reflect country chosen

--- assets/js/image-change.js
+++ assets/js/image-change.js
@@ -1,17 +1,23 @@
 function changeImageOne() {
     document.getElementById("changing-image").innerHTML='<img src="assets/images/indonesia.png">';
-} 
+    document.getElementById("country-name").innerHTML="Indonesia";
+
+};
 
 function changeImageTwo() {
     document.getElementById("changing-image").innerHTML='<img src="assets/images/singapore.png">';
+    document.getElementById("country-name").innerHTML="Singapore";
     
-} 
+};
 
 function changeImageThree() {
     document.getElementById("changing-image").innerHTML='<img src="assets/images/thailand.png">';
+    document.getElementById("country-name").innerHTML="Thailand";
     
-} 
+};
 
 function changeImageFour() {
     document.getElementById("changing-image").innerHTML='<img src="assets/images/malaysia.png">';
-} 
+    document.getElementById("country-name").innerHTML="Malaysia"
+
+};
