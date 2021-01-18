0631e45e5458cc0788fd310a36cf9f72803729f5
EllyC20 <callanelly@yahoo.com>
2021-01-18 16:17:55 +0000

    Add more flight data, Thailand and Malaysia

--- assets/js/skyscanner.js
+++ assets/js/skyscanner.js
@@ -85,11 +85,15 @@ function thailandFlights() {
             console.log(json);
             document.getElementById("text-change").innerHTML = "Find Out Below:";
             document.getElementById("carriers-one").innerHTML = json.Carriers[0].Name;
-            document.getElementById("carriers-two").innerHTML = json.Carriers[0].Name;
+            document.getElementById("carriers-two").innerHTML = json.Carriers[3].Name;
             document.getElementById("from-one").innerHTML = json.Places[1].CityName;
+            document.getElementById("from-two").innerHTML = json.Places[1].CityName;
             document.getElementById("to-one").innerHTML = json.Places[0].CityName;
+            document.getElementById("to-two").innerHTML = json.Places[0].CityName;
             document.getElementById("quotes-one").innerHTML = json.Quotes[0].MinPrice;
+            document.getElementById("quotes-two").innerHTML = json.Quotes[12].MinPrice;
             document.getElementById("direct-one").innerHTML = json.Quotes[0].Direct;
+            document.getElementById("direct-two").innerHTML = json.Quotes[0].Direct;
         })
         .catch(error => {
             console.log("Something went wrong - error!");
@@ -115,11 +119,16 @@ function malaysiaFlights() {
         .then(json => {
             console.log(json);
             document.getElementById("text-change").innerHTML = "Find Out Below:";
-            document.getElementById("carriers").innerHTML = json.Carriers[0].Name;
-            document.getElementById("from").innerHTML = json.Places[0].CityName;
-            document.getElementById("to").innerHTML = json.Places[1].CityName;
-            document.getElementById("quotes").innerHTML = json.Quotes[0].MinPrice;
-            document.getElementById("direct").innerHTML = json.Quotes[0].Direct;
+            document.getElementById("carriers-one").innerHTML = json.Carriers[0].Name;
+            document.getElementById("carriers-two").innerHTML = json.Carriers[1].Name;
+            document.getElementById("from-one").innerHTML = json.Places[0].CityName;
+            document.getElementById("from-two").innerHTML = json.Places[0].CityName;
+            document.getElementById("to-one").innerHTML = json.Places[1].CityName;
+            document.getElementById("to-two").innerHTML = json.Places[1].CityName;
+            document.getElementById("quotes-one").innerHTML = json.Quotes[0].MinPrice;
+            document.getElementById("quotes-two").innerHTML = json.Quotes[9].MinPrice;
+            document.getElementById("direct-one").innerHTML = json.Quotes[0].Direct;
+            document.getElementById("direct-two").innerHTML = json.Quotes[9].Direct;
         })
         .catch(error => {
             console.log("Something went wrong - error!");
