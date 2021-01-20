021ab9c7d57d16d916736753db55090c39b4b192
EllyC20 <callanelly@yahoo.com>
2021-01-20 15:27:25 +0000

    Add comments describing functions

--- assets/js/image-change.js
+++ assets/js/image-change.js
@@ -1,22 +1,23 @@
+// Add event listener to Indonesia button to change image element and add text 
 document.getElementById("indo-btn").addEventListener("click", function() {
  
     document.getElementById("changing-image").innerHTML='<img src="assets/images/indonesia.png">';
     document.getElementById("country-name").innerHTML="Indonesia";
 
 });
-
+// Add event listener to Singapore button to change image element and add text 
 document.getElementById("singapore-btn").addEventListener("click", function() {
 
     document.getElementById("changing-image").innerHTML='<img src="assets/images/singapore.png">';
     document.getElementById("country-name").innerHTML="Singapore"; 
 });
-
+// Add event listener to Thailand button to change image element and add text 
 document.getElementById("thai-btn").addEventListener("click", function() {
 
     document.getElementById("changing-image").innerHTML='<img src="assets/images/thailand.png">';
     document.getElementById("country-name").innerHTML="Thailand";
 });
-
+// Add event listener to Malaysia button to change image element and add text 
 document.getElementById("malaysia-btn").addEventListener("click", function() {
 
     document.getElementById("changing-image").innerHTML='<img src="assets/images/malaysia.png">';
