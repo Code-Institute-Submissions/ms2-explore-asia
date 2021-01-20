a9c3d16bf4fb1ff5c20fe1634821ff8c9fd6fa82
EllyC20 <callanelly@yahoo.com>
2021-01-20 14:08:56 +0000

    Add comments to file

--- assets/js/country-data-api.js
+++ assets/js/country-data-api.js
@@ -1,10 +1,11 @@
 // CREDIT API ENDPOINTS PROVIDED BY https://restcountries.eu/
 
 //Indonesia Data
+// Add event listener
 document.getElementById("indo-btn").addEventListener("click" , indonesiaData);
 
 function indonesiaData(){
-    
+// Make fetch call
     fetch("https://restcountries.eu/rest/v2/alpha?codes=id",
     {
         "method": "GET"
@@ -12,7 +13,7 @@ function indonesiaData(){
 
     .then(response => {
         return response.json();
-
+// Display data
     })
     .then(json => {
         document.getElementById("capital-text").innerHTML = json[0].capital;
@@ -21,17 +22,18 @@ function indonesiaData(){
         document.getElementById("language-text").innerHTML = json[0].languages[0].nativeName;
         
     })
-
+// User message for if API fails / error
     .catch(error => {
         document.getElementById("country-name").innerHTML = "Sorry this information is not available right now. Please try again later."
     });
 
 }
 //Singapore Data
+// Add event listener
 document.getElementById("singapore-btn").addEventListener("click" , singaporeData);
 
 function singaporeData(){
-    
+// Make fetch call
     fetch("https://restcountries.eu/rest/v2/alpha?codes=sg",
     {
         "method": "GET"
@@ -41,23 +43,25 @@ function singaporeData(){
         return response.json();
 
     })
+// Display data
     .then(json => {
         document.getElementById("capital-text").innerHTML = json[0].capital;
         document.getElementById("currency-text").innerHTML = json[0].currencies[0].name;
         document.getElementById("population-text").innerHTML = json[0].population;
         document.getElementById("language-text").innerHTML = json[0].languages[1].nativeName;
     })
-
+// User message for if API fails / error
     .catch(error => {
         document.getElementById("country-name").innerHTML = "Sorry this information is not available right now. Please try again later."
     });
 
 }
 //Thailand Data
+// Add event listener
 document.getElementById("thai-btn").addEventListener("click" , thailandData);
 
 function thailandData(){
-    
+// Make fetch call
     fetch("https://restcountries.eu/rest/v2/alpha?codes=th",
     {
         "method": "GET"
@@ -67,23 +71,25 @@ function thailandData(){
         return response.json();
 
     })
+// Display data
     .then(json => {
         document.getElementById("capital-text").innerHTML = json[0].capital;
         document.getElementById("currency-text").innerHTML = json[0].currencies[0].name;
         document.getElementById("population-text").innerHTML = json[0].population;
         document.getElementById("language-text").innerHTML = json[0].languages[0].name;
     })
-
+// User message for if API fails / error
     .catch(error => {
         document.getElementById("country-name").innerHTML = "Sorry this information is not available right now. Please try again later."
     });
 
 }
 //Malaysia Data
+// Add event listener
 document.getElementById("malaysia-btn").addEventListener("click" , malaysiaData);
 
 function malaysiaData(){
-    
+// Make fetch call
     fetch("https://restcountries.eu/rest/v2/alpha?codes=my",
     {
         "method": "GET"
@@ -93,13 +99,14 @@ function malaysiaData(){
         return response.json();
 
     })
+// Display data
     .then(json => {
         document.getElementById("capital-text").innerHTML = json[0].capital;
         document.getElementById("currency-text").innerHTML = json[0].currencies[0].name;
         document.getElementById("population-text").innerHTML = json[0].population;
         document.getElementById("language-text").innerHTML = json[0].languages[0].name;
     })
-
+// User error message for if API fails / error
     .catch(error => {
         document.getElementById("country-name").innerHTML = "Sorry this information is not available right now. Please try again later."
     });
